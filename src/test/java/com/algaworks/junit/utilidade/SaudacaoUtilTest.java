1463a86c59338af25ceefc309438e216bc3cc19e
soapmactavish23 <ricknogueira1231@gmail.com>
2024-12-01 10:52:51 -0300

    2.2. Escrevendo o seu primeiro teste unitário

---
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -0,0 +1,15 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaudacaoUtilTest {
+
+    @Test
+    void saudar() {
+        String saudacao = SaudacaoUtil.saudar(9);
+        Assertions.assertTrue(saudacao.equals("Bom dia"));
+    }
+}
