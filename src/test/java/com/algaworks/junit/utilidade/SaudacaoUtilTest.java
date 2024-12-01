f4dd6855c81d9ea9fc3e36d1097ff38321f7c350
soapmactavish23 <ricknogueira1231@gmail.com>
2024-12-01 11:10:31 -0300

    2.5. Asserções em listas

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,6 +1,5 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,6 +9,16 @@ class SaudacaoUtilTest {
     @Test
     void saudar() {
         String saudacao = SaudacaoUtil.saudar(9);
-        Assertions.assertTrue(saudacao.equals("Bom dia"));
+        assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    }
+
+    @Test
+    void deveLancarException() {
+        assertThrows(IllegalArgumentException.class, () -> SaudacaoUtil.saudar(-10));
+    }
+
+    @Test
+    void naoDeveLancarException() {
+        assertDoesNotThrow(() -> SaudacaoUtil.saudar(0));
     }
 }
